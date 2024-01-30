ef43fa91f789a72fa8c77f2300013cf9dcb96924
Matthew Chanco <Matthew.Chanco@fdmgroup.com>
2024-01-30 08:37:00 +0800

    Initial commit

---
+++ src/main/java/com/fdmgroup/MattBadmintonMatchmaker/MattBadmintonMatchmakerApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.MattBadmintonMatchmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattBadmintonMatchmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattBadmintonMatchmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/MattBadmintonMatchmaker/MattBadmintonMatchmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.MattBadmintonMatchmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattBadmintonMatchmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
